59252db666f6d13d991dac03238ef4af2dd4a545
Eduard <mamaxropela@gmail.com>
2015-03-11 15:58:06 +0200

    add properties in card model

--- Hospital/HospitalLibrary/Card.cs
+++ Hospital/HospitalLibrary/Card.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace HospitalLibrary
 {
-	class Card
+	public class Card
 	{
+		#region Properties
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public SexType Sex { get; set; }
+		public string Note { get; set; }
+		public bool IsAgain { get; set; }
+
+		#endregion
+
+		#region Navigation Properties
+
+		public virtual IEnumerable<Note> Notes { get; set; }
+		public virtual IEnumerable<Session> Sessions { get; set; } 
+
+		#endregion
 	}
 }
