bd7ca4d293c3f64b1077c214a31ed3209310ca2c
Eduard <mamaxropela@gmail.com>
2015-03-11 16:08:28 +0200

    create entity context model

---
+++ Hospital/HospitalLibrary/HospitalContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary
+{
+	public class HospitalContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Note> Notes { get; set; }
+		public DbSet<Card> Cards { get; set; }
+		public DbSet<Session> Sessions { get; set; }
+		public DbSet<Diagnosis> Diagnoses { get; set; }
+	}
+}
