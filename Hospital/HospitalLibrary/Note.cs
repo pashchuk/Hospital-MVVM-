bb61527ea8193d3952cbe6ee1626ed1568f22f0a
Eduard <mamaxropela@gmail.com>
2015-03-11 15:51:28 +0200

    add properties to Note model

--- Hospital/HospitalLibrary/Note.cs
+++ Hospital/HospitalLibrary/Note.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace HospitalLibrary
 {
-	class Note
+	public class Note
 	{
+		#region Properties
+
+		public int Id { get; set; }
+		public bool IsHidden { get; set; }
+		public DateTime Date { get; set; }
+		public string NoteText { get; set; }
+
+		#endregion
+
+		#region Navigation Properties
+
+		public virtual User Author { get; set; }
+		public virtual Card Card { get; set; }
+
+		#endregion
 	}
 }
