cb3d6345e444f03ee06db867885cc366dfc2b1ba
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-05-29 23:22:03 +0300

    implement INotifyPropertyChanged pattern

---
+++ Hospital/DesktopApp/ViewModel/CardViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopApp.Model;
+
+namespace DesktopApp.ViewModel
+{
+	public class CardViewModel : ViewModelBase
+	{
+		private Card _card;
+
+		public string Name
+		{
+			get { return _card.Patient.FirstName; }
+			set
+			{
+				_card.Patient.FirstName = value;
+				OnPropertyChanged("Name");
+			}
+		}
+		public int Age
+		{
+			get { return _card.Patient.Age; }
+			set
+			{
+				_card.Patient.Age = value;
+				OnPropertyChanged("Age");
+			}
+		}
+		public string Sex
+		{
+			get { return _card.Patient.Sex; }
+			set
+			{
+				_card.Patient.Sex = value;
+				OnPropertyChanged("Sex");
+			}
+		}
+		public CardViewModel(Card card)
+		{
+			_card = card;
+		}
+	}
+}
--- Hospital/DesktopApp/ViewModel/WorkWindowViewModel.cs
+++ Hospital/DesktopApp/ViewModel/WorkWindowViewModel.cs
@@ -1,13 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesktopApp.Model;
+using DesktopApp.View;
 
 namespace DesktopApp.ViewModel
 {
-	public class WorkWindowViewModel
+	public class WorkWindowViewModel : ViewModelBase
 	{
+		public WorkWindowViewModel()
+		{
+			using (var db = new HospitalContext())
+			{
+//				db.Cards.Load();
+//				
+//
+//				var patient1 = new Patient()
+//				{
+//					Address = "Kyiv, Ukraine",
+//					Age = 20,
+//					Email = "as@asd.asd",
+//					FirstName = "Левицький",
+//					LastName = "Віталій",
+//					MiddleName = "Андрійович",
+//					Sex = "Male",
+//					Phone = "0961234567",
+//					Password = "1111"
+//				};
+//				var patient2 = new Patient()
+//				{
+//					Address = "Kyiv, Ukraine",
+//					Age = 20,
+//					Email = "as@asd.asd",
+//					FirstName = "Лddfdий",
+//					LastName = "dfdf",
+//					MiddleName = "Аdfdfч",
+//					Sex = "Male",
+//					Phone = "0961234567",
+//					Password = "1111"
+//				};
+//				var doc = new Doctor()
+//				{
+//					Address = "Kyiv, Ukraine",
+//					Age = 20,
+//					Email = "as@asd.asd",
+//					FirstName = "Пащук",
+//					LastName = "Едуард",
+//					MiddleName = "Федорович",
+//					Sex = "Male",
+//					Phone = "0961234567",
+//					Password = "1111",
+//					Office = "travmatolog"
+//				};
+//				var card = new Card() {Patient = patient1, Name = "Temp"};
+//				var card2 = new Card() { Patient = patient2, Name = "Temp2"};
+//				doc.Cards.Add(card);
+//				doc.Cards.Add(card2);
+//				db.Cards.Add(card);
+//				db.Cards.Add(card2);
+//				db.Doctors.Add(doc);
+//				db.SaveChanges();
+
+				db.Cards.Load();
+				db.Users.Load();
+
+				CardsViews = new ObservableCollection<CardView>();
+				foreach (var card in db.Cards)
+					CardsViews.Add(new CardView() {DataContext = new CardViewModel(card)});
+			}
+		}
 		public string UserName { get; set; }
+
+		private ObservableCollection<CardView> _cardsViews;
+		public ObservableCollection<CardView> CardsViews
+		{
+			get { return _cardsViews; }
+			set
+			{
+				_cardsViews = value;
+				OnPropertyChanged("CardsViews");
+			}
+		}
+		
 	}
 }
