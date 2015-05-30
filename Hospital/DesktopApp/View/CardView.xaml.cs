0c581d66f22cc2339c0e342b80766139d56b335e
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-05-30 15:03:18 +0300

    add button styles

--- Hospital/DesktopApp/View/CardView.xaml.cs
+++ Hospital/DesktopApp/View/CardView.xaml.cs
@@ -24,32 +24,5 @@ namespace DesktopApp.View
 		{
 			InitializeComponent();
 		}
-
-		public static readonly DependencyProperty UserNameProperty = DependencyProperty.Register(
-			"UserName", typeof (string), typeof (CardView), new PropertyMetadata(default(string)));
-
-		public string UserName
-		{
-			get { return (string) GetValue(UserNameProperty); }
-			set { SetValue(UserNameProperty, value); }
-		}
-
-		public static readonly DependencyProperty UserSexProperty = DependencyProperty.Register(
-			"UserSex", typeof (string), typeof (CardView), new PropertyMetadata(default(string)));
-
-		public string UserSex
-		{
-			get { return (string) GetValue(UserSexProperty); }
-			set { SetValue(UserSexProperty, value); }
-		}
-
-		public static readonly DependencyProperty UserAgeProperty = DependencyProperty.Register(
-			"UserAge", typeof (int), typeof (CardView), new PropertyMetadata(default(int)));
-
-		public int UserAge
-		{
-			get { return (int) GetValue(UserAgeProperty); }
-			set { SetValue(UserAgeProperty, value); }
-		}
 	}
 }
