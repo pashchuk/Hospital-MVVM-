536b50705600868d0f7ca46b02204e7e1128eb10
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-06-01 22:22:43 +0300

    create new viewmodel for session

---
+++ Hospital/DesktopApp/ViewModel/SessionViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopApp.Model;
+
+namespace DesktopApp.ViewModel
+{
+	class SessionViewModel :ViewModelBase
+	{
+		private Session _session;
+		private bool _state;
+
+		public int Id { get; private set; }
+
+		public string Diagnosis
+		{
+			get { return _session.Diagnosis.Description; }
+			set
+			{
+				_session.Diagnosis.Description = value;
+				OnPropertyChanged("Diagnosis");
+			}
+		}
+
+		public bool State
+		{
+			get { return _state; }
+			set
+			{
+				_state = value;
+				OnPropertyChanged("State");
+			}
+		}
+
+		public DateTime Date
+		{
+			get { return _session.Date; }
+		}
+
+		public Session GetSession()
+		{
+			return _session;
+		}
+
+		public SessionViewModel(Session session, int id)
+		{
+			_session = session;
+			Id = id;
+			_state = false;
+		}
+
+		public SessionViewModel(Session session) : this(session, 1)
+		{
+		}
+	}
+}
